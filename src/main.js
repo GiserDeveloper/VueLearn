90f9b54722ccdbc61d845f3ed28f0c91a398abb2
GiserDeveloper <864443272@qq.com>
2019-08-26 16:34:51 +0800

    day2 更新

--- src/main.js
+++ src/main.js
@@ -8,11 +8,22 @@ import VueRouter from 'vue-router'
 //安装路由
 Vue.use(VueRouter)
 
+//导入moment.js
+import moment from 'moment'
+
+//定义全局的过滤器
+Vue.filter('dateFormat',function(dataStr,pattern="YYYY-MM-DD HH:mm:ss"){
+    return moment(dataStr).format(pattern)
+})
+
 //导入 vue-resource
 import VueResource from 'vue-resource'
 
 //安装 vue-resource
 Vue.use(VueResource)
+
+//设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005'
 
 
 //导入MUI的样式
@@ -20,11 +31,12 @@ import './lib/mui/css/mui.min.css';
 import './lib/mui/css/icons-extra.css';
 
 //按需导入Mint-UI中的组件
-import {Header , Swipe, SwipeItem} from 'mint-ui';
+import {Header , Swipe, SwipeItem, Button } from 'mint-ui';
 //注册组件
 Vue.component(Header.name , Header);
 Vue.component(Swipe.name , Swipe);
 Vue.component(SwipeItem.name , SwipeItem);
+Vue.component(Button.name , Button);
 
 //导入自己的router-js路由模块
 import router from './router.js'
